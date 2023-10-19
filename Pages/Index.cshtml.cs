345968b6bc8b8f670515b5e2c9ec7bbaf8aca80c
Brian Jaikens <brian_jaikens@hotmail.com>
2023-10-19 12:41:10 +0100

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Basic_Web_App.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IWebHostEnvironment _environment;
+
+        public IndexModel(ILogger<IndexModel> logger,
+            IWebHostEnvironment environment)
+        {
+            _logger = logger;
+            _environment = environment;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPostDownloadFile()
+        {
+            const string testFile = "test.txt";
+            string rootPath = _environment.WebRootPath + "\\tempfiles\\";
+
+            string filePath = System.IO.Path.Combine(rootPath, testFile);
+
+            return PhysicalFile(filePath, "text/plain", testFile);
+        }
+    }
+}
